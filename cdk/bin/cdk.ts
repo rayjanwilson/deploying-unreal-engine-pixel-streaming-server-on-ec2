e70fe902b3ce3c90bf2abbe72c86244c9ae94882
Rayjan Wilson <rayjwil@amazon.com>
2021-04-30 11:59:15 -0400

    first draft of inception pipeline

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,10 +3,14 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { NetworkStack } from '../lib/network-stack';
 import { ImageBuilderStack } from '../lib/imagebuilder-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
+
 const network = new NetworkStack(app, 'UEPSNetworkStack');
+
 new ImageBuilderStack(app, 'UEPSBuilderStack', {
   vpc: network.vpc
-})
-app.synth();
+});
+
+new PipelineStack(app, 'UEPSPipelineStack');
---
+++ cdk/lib/pipeline-stack.ts
@@ -0,0 +1,76 @@
+import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
+import * as cb from "@aws-cdk/aws-codebuild";
+import * as cp from "@aws-cdk/aws-codepipeline";
+import * as cpa from "@aws-cdk/aws-codepipeline-actions";
+
+export class PipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const sourceArtifact = new cp.Artifact();
+        const cdkBuildOutput = new cp.Artifact();
+
+        // CodeBuild Project
+        const cdkBuildProject = new cb.PipelineProject(this, "CdkBuildProject", {
+            buildSpec: cb.BuildSpec.fromObject({
+                version: "0.2",
+                phases: {
+                    install: {
+                        "runtime-versions": { nodejs: 14 },
+                        commands: ["cd cdk", "npm install"],
+                    },
+                    build: {
+                        commands: ["npm run build", "npm run cdk synth --"],
+                    },
+                },
+                artifacts: {
+                    'base-directory': 'cdk.out/',
+                    files: [`${this.stackName}.template.json`]
+                }
+            }),
+            environment: {
+                buildImage: cb.LinuxBuildImage.STANDARD_5_0
+            }
+        });
+        // Actions
+        const sourceAction = new cpa.GitHubSourceAction({
+            actionName: "GitHubSource",
+            output: sourceArtifact,
+            oauthToken: SecretValue.secretsManager("github-token"),
+            owner: "rayjanwilson",
+            repo: "deploying-unreal-engine-pixel-streaming-server-on-ec2",
+            branch: '12-trigger-from-sns',
+            trigger: cpa.GitHubTrigger.WEBHOOK
+        });
+
+        new cp.Pipeline(this, 'BuildPipeline', {
+            stages: [
+                {
+                    stageName: 'Source',
+                    actions: [ sourceAction]
+                },
+                {
+                    stageName: 'BuildAndAdministerPipeline',
+                    actions: [
+                        new cpa.CodeBuildAction({
+                            actionName: 'CDK_Build',
+                            project: cdkBuildProject,
+                            input: sourceArtifact,
+                            outputs: [ cdkBuildOutput ],
+                            runOrder: 1
+                        }),
+                        new cpa.CloudFormationCreateUpdateStackAction({
+                            actionName: "AdministerPipeline",
+                            templatePath: cdkBuildOutput.atPath(`${this.stackName}.template.json`),
+                            stackName: this.stackName,
+                            adminPermissions: true,
+                            runOrder: 2,
+                        })
+                    ]
+                }
+            ],
+            restartExecutionOnUpdate: true
+        })
+
+    }
+}
